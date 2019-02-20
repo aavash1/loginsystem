da80d64e7e6a2d55da3e389928db09cd8c41fcab
aavash1 <aavashbhandari@gmail.com>
2019-02-21 01:44:11 +0900

    New Commit! Sign Up, login, logout Success

--- header.php
+++ header.php
@@ -19,16 +19,18 @@ session_start();
 				<div class="nav-login">
 					<?php
 					if(isset($_SESSION['u_id'])){
-						echo '<form action="includes/logout.inc.php" method="POST"><button name="submit" type="submit">Logout</button>
+						echo '<form action="includes/logout.inc.php" method="POST">
+						<button type="submit" name="submit">Logout</button>
 						</form>';
 
 					}
 					else{
 						echo'<form action="includes/login.inc.php" method="POST">
-						<input type="text" name="uid" placeholder="Username/email">
+						<input type="text" name="uname" placeholder="Username/email">
 						<input type="password" name="pwd" placeholder="password">
 						<button type="submit" name="submit">Login</button>
-					</form>';
+						</form>
+						<a href="signup.php">Sign Up</a>';
 
 					}
 					?>
